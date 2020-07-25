25a7c9871c241885111b0371eb25ae6ce4373c8f
omarion3698 <omaribinbakarivic@gmail.com>
2020-07-25 17:24:49 +0300

    Last update

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,12 @@ var multiply = function(number1, number2) {
 var divide = function(number1, number2) {
   return number1 / number2;
 };
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-alert(add(number1, number2));
+$(document).ready(function() {
+  $("form#add").submit(function(event){
+    event.preventDefault();
+  var number1 = parseInt($("#add1").val());
+  var number2 = parseInt($("#add2").val());
+  var result = add(number1, number2);
+  $("#output").text(result);
+});
+});
